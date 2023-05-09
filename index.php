9b49a5d3725a6d81cfa0f2a228042c9923a8683a
DagdAA <120112292+DagdAA@users.noreply.github.com>
2023-05-09 22:15:36 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ejemplo de página de inicio en PHP</title>
+</head>
+<body>
+	<h1>Bienvenido a mi página de inicio en PHP</h1>
+	<p>Esta es una página de ejemplo en PHP con un título y un apartado de texto.</p>
+	<footer>
+		<p>Derechos de autor © 2023 - Todos los derechos reservados</p>
+	</footer>
+</body>
+</html>
